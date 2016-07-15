d91407c7e5628c0a4461502d2f6e10e360ea46e8
Nicolas <nicolas.marniesse@gmail.com>
2016-07-15 21:29:53 +0200

    feat(style): css work in progress

--- src/application/views/header.php
+++ src/application/views/header.php
@@ -16,6 +16,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" href="assets/css/header.css">
+	<link rel="stylesheet" href="assets/css/body.css">
 
 </head>
 <body>
--- src/application/views/index.php
+++ src/application/views/index.php
@@ -18,16 +18,41 @@
 				<img id="qr_code" src="assets/img/qr_code.jpg" alt="avatar" class="img-thumbnail">
 			</div>
 			<div class="col-xs-9">
-				
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam enim nunc, blandit sit amet est nec, commodo interdum purus. Mauris congue metus orci, eget luctus tellus pharetra vulputate. Maecenas tellus sem, pulvinar a dui sit amet, ornare euismod ligula. Aliquam semper ac odio in rhoncus. Sed aliquet venenatis neque id placerat. Maecenas laoreet sollicitudin velit, nec aliquam massa dapibus a. Suspendisse viverra sagittis enim. Donec id iaculis mauris. Curabitur bibendum mollis libero, ac eleifend nulla. Curabitur ultricies ligula sit amet libero blandit, vel venenatis nisi laoreet. Sed tincidunt urna tempor varius feugiat.
 
-Cras volutpat efficitur quam, nec luctus eros tempor et. Mauris erat tellus, blandit mollis euismod ac, volutpat a felis. Duis elementum tellus eros, eget fermentum odio convallis sit amet. Proin vestibulum, erat nec semper iaculis, urna est dapibus nunc, vitae efficitur mauris justo sodales arcu. Suspendisse cursus justo sit amet eros eleifend convallis. Quisque hendrerit, odio sed iaculis fringilla, velit lectus sollicitudin turpis, et porta arcu ipsum ac nunc. In maximus sodales mi luctus ultrices.
+				<div class="section">
+					<h1>Expériences</h1>
+					<div class="item well">
+						<h2>
+							<span class="item-title">Responsable SI et Ingénieur développeur web full-stack</span>
+							<span class="item-info">Naoned Systèmes, Depuis 2012</span>
+						</h2>
+						<div>
 
-Curabitur consequat felis vitae bibendum consectetur. Praesent turpis nibh, elementum at risus consectetur, congue dictum nibh. Fusce bibendum molestie semper. Fusce tortor lectus, fermentum et aliquam sed, fermentum a odio. Etiam laoreet quam dui, sit amet elementum sapien sodales tincidunt. Mauris tempor suscipit massa quis aliquam. Nunc quis purus sit amet tellus fermentum sollicitudin. Cras molestie venenatis nunc at semper.
+						</div>
+					</div>
+					<div class="item well">
+						<h2>
+							<span class="item-title">Développeur</span>
+							<span class="item-info">Muséomix, octobre 2014</span>
+						</h2>
+						<div>
+							<ul>
+								<li>Co-création en 3 jours d'un projet numérique de médiation muséale</li>
+								<li>Participation à l'élaboration du projet de A à Z</li>
+								<li>Développement d'un prototype en PHP, JQuery, Bootstrap</li>
+							</ul>
+						</div>
+					</div>
+					<div class="item well">
+						<h2>
+							<span class="item-title">Responsable Informatique</span>
+							<span class="item-info">De La Rivière International, 2007-2011</span>
+						</h2>
+						<div>
 
-Praesent laoreet ligula in lacus rutrum blandit. Nam consequat lacus sed dictum pellentesque. Sed sagittis suscipit ante, quis rhoncus velit vestibulum sed. Aenean ut posuere velit. Sed quam nulla, eleifend laoreet rhoncus vel, laoreet eu libero. Maecenas eget pretium sem, quis ultrices est. Quisque mauris magna, molestie eu bibendum at, viverra non neque. Integer tincidunt non mauris non semper. Cras ligula erat, hendrerit ut efficitur eu, elementum ut massa. Maecenas eleifend nisl a justo dictum, eget euismod erat viverra.
-
-Nunc cursus nisi non lacinia sollicitudin. Phasellus elit risus, venenatis nec fringilla sit amet, rhoncus nec leo. Quisque tincidunt lacinia nulla in sagittis. Mauris congue purus id sapien imperdiet auctor. Cras commodo ex massa, vitae lobortis diam venenatis quis. Curabitur vitae tellus ultrices, pellentesque metus at, eleifend eros. Pellentesque porttitor velit vel eros faucibus facilisis. Aliquam eleifend non sapien eget blandit. Vestibulum convallis libero augue, vel tempus velit euismod id. Maecenas fermentum pretium tellus. In placerat dolor erat, ut condimentum lectus placerat non. Duis erat lorem, laoreet ut quam eu, semper dignissim neque. Morbi condimentum quam eget dolor molestie tempor.
+						</div>
+					</div>
+				</div>
 
 			</div>
 		</div>
