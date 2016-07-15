785b64658987b63441c99ee6226235b01e2e6782
Nicolas <nicolas.marniesse@gmail.com>
2016-07-15 22:29:02 +0200

    feat(style): css work in progress (2)

--- src/application/views/index.php
+++ src/application/views/index.php
@@ -15,10 +15,13 @@
 					nicolas.marniesse@gmail.com<br/>
 					06.82.68.08.98<br/>
 				</div>
-				<img id="qr_code" src="assets/img/qr_code.jpg" alt="avatar" class="img-thumbnail">
+				<div class="presentation">
+					<h3>CV en ligne</h3>
+					<img id="qr_code" src="assets/img/qr_code.jpg" alt="avatar" class="img-thumbnail">
+				</div>
 			</div>
 			<div class="col-xs-9">
-
+				<h1 id="page-title">Nicolas Marniesse</h1>
 				<div class="section">
 					<h1>Expériences</h1>
 					<div class="item well">
@@ -27,7 +30,30 @@
 							<span class="item-info">Naoned Systèmes, Depuis 2012</span>
 						</h2>
 						<div>
-
+							<h3>Création d'un nouveau logiciel web</h3>
+							<ul>
+								<li>Création de l'architecture logicielle</li>
+								<li>PHP Symfony</li>
+								<li>AngularJS</li>
+								<li>Création API REST</li>
+								<li>PostgreSQL, POMM (http://www.pomm-project.org/)</li>
+								<li>Elasticsearch</li>
+								<li>Tests PHPUnit, déploiement, docker, documentation</li>
+								<li>Méthode SCRUM</li>
+							</ul>
+							<h3>Maintenance et développement sur un logiciel web</h3>
+							<ul>
+								<li>Développement web (PHP, C++, Javascript-JQuery, HTML, CSS, XML)</li>
+								<li>Mise en place du déploiement</li>
+								<li>Méthode SCRUM</li>
+								<li>Documentation</li>
+							</ul>
+							<h3>Responsable Système d'Information</h3>
+							<ul>
+								<li>Pilotage et réalisation de projets</li>
+								<li>Gestion budgétaire</li>
+								<li>Gestion du parc informatique : serveurs et postes</li>
+							</ul>
 						</div>
 					</div>
 					<div class="item well">
@@ -38,19 +64,54 @@
 						<div>
 							<ul>
 								<li>Co-création en 3 jours d'un projet numérique de médiation muséale</li>
-								<li>Participation à l'élaboration du projet de A à Z</li>
 								<li>Développement d'un prototype en PHP, JQuery, Bootstrap</li>
 							</ul>
 						</div>
 					</div>
 					<div class="item well">
 						<h2>
-							<span class="item-title">Responsable Informatique</span>
+							<span class="item-title">Responsable Informatique, Administrateur Systèmes et Réseaux</span>
 							<span class="item-info">De La Rivière International, 2007-2011</span>
 						</h2>
 						<div>
-
+							<h3>Administration systèmes et réseaux</h3>
+							<ul>
+								<li>Installation et gestion de serveurs mail, comptabilité, et Site Web (LAMP)</li>
+								<li>Gestion du parc Windows et Linux</li>
+							</ul>
+							<h3>Développements</h3>
+							<ul>
+								<li>Développements Web : PHP, Javascript, MySQL</li>
+								<li>VBS/VBA et bash</li>
+								<li>Développement et création d'indicateurs d'entreprise</li>
+								<li>Développement de connecteurs entre les briques logicielles du SI</li>
+							</ul>
+							<h3>Autres</h3>
+							<ul>
+								<li>Pilotage projets</li>
+								<li>Déplacements professionnels dans les filiales en Chine et en Tunisie</li>
+							</ul>
 						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<h1>Formations</h1>
+					<div class="item well">
+						<ul>
+							<li>
+								<span class="formation-title">Master 2 Systèmes Distribués, Réseaux et Parallélisme</span>
+								<span class="formation-info">Université Bordeaux 1 Sciences et Technologies, 2006</span>
+							</li>
+							<li>
+								<span class="formation-title">Licence Informatique</span>
+								<span class="formation-info">Université Bordeaux 1 Sciences et Technologies, 2004</span>
+							</li>
+							<li>
+								<span class="formation-title">Deug Mathématiques et Informatique Appliqués aux Sciences</span>
+								<span class="formation-info">Université Bordeaux 1 Sciences et Technologies, 2003</span>
+							</li>
+						</ul>
 					</div>
 				</div>
 
